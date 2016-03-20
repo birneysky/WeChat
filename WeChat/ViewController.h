7edc1cd06a64e0b6aa16f3cccd2045fd905c94b6
yingwei0831 <birneysky@163.com>
2016-03-20 18:28:50 +0800

    modify

---
+++ WeChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeChat
+//
+//  Created by birneysky on 16/3/20.
+//  Copyright © 2016年 birneysky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WeChat/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WeChat
+//
+//  Created by birneysky on 16/3/20.
+//  Copyright © 2016年 birneysky. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
